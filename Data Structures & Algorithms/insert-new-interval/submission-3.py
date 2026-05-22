fa1134fd3e33fa2d937ddfc7d65520f218a0b5cf
dhivya-n <25dhivya@gmail.com>
2026-05-22 21:16:00 +0530

    Add: insert-new-interval - submission-3

---
+++ Data Structures & Algorithms/insert-new-interval/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+        i = 0
+        n = len(intervals)
+        if n == 0:
+            return [newInterval]
+        while i < n and intervals[i][0] < newInterval[0] and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i+=1
+
+        start = min(intervals[i][0], newInterval[0]) if i < n else newInterval[0]
+        found = False
+        while i < n and not found:
+            if intervals[i][0] > newInterval[1]:
+                result.append([start, newInterval[1]])
+                found = True
+            elif intervals[i][1] > newInterval[1]:
+                result.append([start, intervals[i][1]])
+                i+=1
+                found = True
+            else:
+                i+=1
+        if not found:
+            result.append([start, newInterval[1]])     
+        while i < n:
+            result.append(intervals[i])
+            i+=1
+        return result
+
+        
+
+           
+        
